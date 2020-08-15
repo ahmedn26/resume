52b276fa94ff5353f28db793ef3035cf3e089c0a
ahmedn26 <54607846+ahmedn26@users.noreply.github.com>
2020-08-15 04:20:56 -0500

    C# Project
    A Windows Form app that demonstrates the Game of Life using C# and GUI.

---
+++ Game of Life/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Life
+{
+    public partial class Form1 : Form
+    {
+        Generation game = new Generation(); 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("By Nusra Ahmed.");
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            game.DrawGame(e.Graphics);
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            game.PlaceOrganism(e.X / 20, e.Y / 20); //use a CONSTANT (not 20)!!!
+            pictureBox1.Invalidate();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            game.MarkCells(); //figure out who lives/dies
+            game.ApplyMarks(); //kill and spawn 
+            pictureBox1.Invalidate();
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            game.Reset();
+            pictureBox1.Invalidate();
+        }
+
+        private void steadyStateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            game.Reset();
+
+            game.PlaceOrganism(15, 8);
+            game.PlaceOrganism(15, 9);
+            game.PlaceOrganism(16, 8);
+            game.PlaceOrganism(17, 11);
+            game.PlaceOrganism(18, 11);
+            game.PlaceOrganism(18, 10);
+            pictureBox1.Invalidate();
+        }
+
+        private void verticalOscillationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            game.Reset();
+
+            game.PlaceOrganism(15, 8);
+            game.PlaceOrganism(15, 9);
+            game.PlaceOrganism(15, 10);
+            pictureBox1.Invalidate();
+        }
+
+        private void rPentominoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            game.Reset();
+
+            game.PlaceOrganism(16, 8);
+            game.PlaceOrganism(17, 8);
+            game.PlaceOrganism(15, 9);
+            game.PlaceOrganism(16, 9);
+            game.PlaceOrganism(16, 10);
+            pictureBox1.Invalidate();
+        }
+    }
+}
---
+++ Game of Life/Generation.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Life
+{
+    //one cell on the board
+    struct Cell
+    {
+        public bool hasOrganism; //does this square have a life form?
+        public int state; //what will happen to this life form (if present)?
+                          //the various states a cell can be in
+        public const int EMPTY = 0;
+        public const int SURVIVES = 1;
+        public const int DEATH_BY_OVERCROWDING = 2;
+        public const int DEATH_BY_LONELINESS = 3;
+        public const int SPAWNING = 4; //BIRTH
+        public const int UNMARKED = 5;
+        //constructor for a cell
+        public Cell(bool hasOrganism)
+        {
+            this.hasOrganism = hasOrganism;
+            this.state = EMPTY;
+        }
+    }
+
+    //encapsulate the game 
+    class Generation
+    {
+        const int WIDTH = 40;
+        const int HEIGHT = 30;
+
+        Cell[,] grid = new Cell[WIDTH, HEIGHT];
+
+        //count cell's neighbors (figure out what will happen to this cell)
+        public void MarkCells()
+        {
+            for (int i = 0; i < WIDTH; i++)
+            {
+                for (int j = 0; j < HEIGHT; j++)
+                {
+                    int neighbors = CountNeighbors(i, j);
+
+                    //set the state flag based on what will happen to this cell
+                    if (grid[i, j].hasOrganism)
+                    {
+                        if (neighbors == 0 || neighbors == 1)
+                            grid[i, j].state = Cell.DEATH_BY_LONELINESS;
+                        else if (neighbors == 2 || neighbors == 3)
+                            grid[i, j].state = Cell.SURVIVES;
+                        else if (neighbors >= 4 & neighbors <= 8)
+                            grid[i, j].state = Cell.DEATH_BY_OVERCROWDING;
+                    }
+                    else if (neighbors == 3)
+                        grid[i, j].state = Cell.SPAWNING;
+                }
+            }
+        }
+
+        /// <summary>
+        /// kill organisms and spawn new ones
+        /// </summary>
+        public void ApplyMarks()
+        {
+            for (int i = 0; i < WIDTH; i++)
+            {
+                for (int j = 0; j < HEIGHT; j++)
+                {
+                    //kill or spawn based on the state variable
+                    //if dies, set hasOrganism to false
+                    //if spawns, set hasOrganism to true
+                    if (grid[i, j].hasOrganism)
+                    {
+                        if (grid[i, j].state == Cell.SURVIVES)
+                        {
+                            //TO-DO
+                            //put an organism in the grid at i, j
+                            grid[i, j].hasOrganism = true;
+                            grid[i, j].state = Cell.UNMARKED;
+
+                        }
+                        if (grid[i, j].state == Cell.DEATH_BY_LONELINESS || grid[i, j].state == Cell.DEATH_BY_OVERCROWDING)
+                            grid[i, j].hasOrganism = false;
+
+                    }
+                    else if (grid[i, j].state == Cell.SPAWNING)
+                    {
+                        //TO-DO
+                        //put an organism in the grid at i, j
+                        grid[i, j].hasOrganism = true;
+                        grid[i, j].state = Cell.UNMARKED;
+                    }
+                }
+            }
+        }
+
+        public void DrawGame(Graphics g)
+        {
+            for (int i = 0; i < WIDTH; i++)
+            {
+                for (int j = 0; j < HEIGHT; j++)
+                {
+                    if (grid[i, j].hasOrganism)
+                        g.DrawImage(Properties.Resources.life_unmarked, i*20, j*20);
+                    else
+                        g.DrawImage(Properties.Resources.life_empty, i * 20, j * 20);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Place/Unplace a cell on the board
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public void PlaceOrganism(int x, int y )
+        {
+            grid[x, y].hasOrganism = !grid[x, y].hasOrganism;
+        }
+
+        //does cell x,y have an organism
+        int CountNeighbor(int x, int y)
+        {
+            if (x < 0 || y < 0 || x>= WIDTH || y >=HEIGHT)
+                return 0;
+
+            if (grid[x, y].hasOrganism)
+                return 1;
+            else
+                return 0;
+        }
+
+        //find total neighbors
+        int CountNeighbors(int x, int y)
+        {
+            int count = CountNeighbor(x, y - 1) +
+                CountNeighbor(x + 1, y - 1) +
+                CountNeighbor(x + 1, y) +
+                CountNeighbor(x + 1, y + 1) +
+                CountNeighbor(x, y + 1) +
+                CountNeighbor(x - 1, y + 1) +
+                CountNeighbor(x - 1, y) +
+                CountNeighbor(x - 1, y - 1);
+
+            return count;
+        }
+
+        //reset the grid
+        public void Reset() {
+            for (int i = 0; i < WIDTH; i++)
+            {
+                for (int j = 0; j < HEIGHT; j++)
+                {
+                    grid[i, j].hasOrganism = false;
+                }
+            }
+        }
+    }
+}
