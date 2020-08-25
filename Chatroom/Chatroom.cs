87080b5579288254997546aa913227ceb69ded45
ahmedn26 <54607846+ahmedn26@users.noreply.github.com>
2020-08-25 06:06:27 -0500

    C# Project
    Chat room TCP Project

---
+++ Chatroom/Chatroom.cs
@@ -0,0 +1,78 @@
+﻿// Nusra Ahmed
+// HW4
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Chatroom
+{
+    class Chatroom
+    {
+       
+
+        static void ReadData(object socket)
+        {
+            TcpClient client = (TcpClient)socket;
+            while (true)
+            {
+                byte[] buffer = new byte[1024];
+                int k = client.GetStream().Read(buffer, 0, buffer.Length);
+                Console.WriteLine("\t\t{0}", Encoding.ASCII.GetString(buffer, 0, k));
+            }
+        }
+
+        public static TcpClient AddConnection()
+        {
+            TcpClient client = new TcpClient();
+            client.Connect(IPAddress.Loopback, 5000);
+            Thread t = new Thread(ReadData);
+            t.Start(client);
+            ChatRoom(ref client);
+            return client;
+
+        }
+
+        static void ChatRoom(ref TcpClient client)
+        {
+            Console.WriteLine("Enter your username: ");
+            string username = Console.ReadLine();
+            // FIX: handle if the user inputs an empty string
+            if (username == "")
+            {
+                // add code to generate random username using Unix time
+                // https://stackoverflow.com/questions/17632584/how-to-get-the-unix-timestamp-in-c-sharp
+                string unixTimestamp = Convert.ToString((DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds);
+                byte[] msg = Encoding.ASCII.GetBytes($"user.{unixTimestamp}");
+                client.GetStream().Write(msg, 0, msg.Length);
+            }
+            else
+            {
+                byte[] msg = Encoding.ASCII.GetBytes(username);
+                client.GetStream().Write(msg, 0, msg.Length);
+            }
+        }
+
+        public static void Message(TcpClient client)
+        {
+            //Console.WriteLine("Please input a string to send");
+            string input = Console.ReadLine();
+            byte[] msg = Encoding.ASCII.GetBytes(input);
+            client.GetStream().Write(msg, 0, msg.Length);
+
+        }
+
+        static void Main(string[] args)
+        {
+            TcpClient client = AddConnection();
+            while (true)
+            {
+                Message(client);
+            }
+        }
+    }
+}
---
+++ Chatroom/Server.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net.NetworkInformation;
+
+namespace Server
+{
+    class Server
+    {
+        //static List<Socket> sockets = new List<Socket>();
+        static Dictionary<string, Socket> sockets = new Dictionary<string, Socket>();
+
+        static void ServerRoom()
+        {
+            TcpListener server = new TcpListener(IPAddress.Loopback, 5000);
+            server.Start();
+
+            Console.WriteLine("Waiting for connection...");
+            while (true)
+            {
+                Thread t = new Thread(handleConnections);
+                Socket client = server.AcceptSocket();
+                t.Start(client);
+            }
+
+            server.Stop();
+
+        }
+        static Socket pingConnections(object socket)
+        { 
+            Socket client = (Socket)socket;
+            Console.WriteLine($"Connection accepted from {client.RemoteEndPoint}");
+            return client;
+
+        }
+
+        static void Main(string[] args)
+        {
+            ServerRoom();
+        }
+
+        static void handleConnections(object socket)
+        {
+            Socket client = pingConnections(socket);
+            byte[] buffer = new byte[1024];
+            int k = client.Receive(buffer);
+            string username = Encoding.ASCII.GetString(buffer, 0, k);
+            sockets[username] = client;
+            while (true)
+            {
+                try
+                {
+                    k = client.Receive(buffer);
+                    foreach (KeyValuePair<string, Socket> otherClient in sockets)
+                    {
+                        if (otherClient.Value != client) //stops the echo
+                        {
+                            string clientData = $"[{username}] - " + Encoding.ASCII.GetString(buffer, 0, k);
+                            otherClient.Value.Send(Encoding.ASCII.GetBytes(clientData));
+                        }
+                    }
+                    Console.WriteLine($"{username} said: " + Encoding.ASCII.GetString(buffer, 0, k));
+                }
+                catch (Exception e)
+                {
+                    sockets.Remove(username);
+                    break;
+                }
+            
+
+            }
+            client.Close();
+        }
+    }
+}
